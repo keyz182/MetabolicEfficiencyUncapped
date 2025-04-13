42f34063082f1e97d7342851a1c4c2b21361c64a
Kieran David Evans <keyz182@gmail.com>
2025-04-13 12:55:38 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/MetabolicEfficiencyUncapped/MetabolicEfficiencyUncappedMod.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace MetabolicEfficiencyUncapped;
+
+public class MetabolicEfficiencyUncappedMod : Mod
+{
+    public static Settings settings;
+
+    public MetabolicEfficiencyUncappedMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.MetabolicEfficiencyUncapped.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "MetabolicEfficiencyUncapped_SettingsCategory".Translate();
+    }
+}
