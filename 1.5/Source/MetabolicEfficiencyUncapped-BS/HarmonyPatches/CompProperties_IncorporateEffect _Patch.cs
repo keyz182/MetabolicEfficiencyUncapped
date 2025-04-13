dbcd0ca7e828ad15b4fa6aa8ff3bb481e7f748a6
Kieran David Evans <keyz182@gmail.com>
2025-04-13 14:56:02 +0100

    Add incorporate patch

---
+++ 1.5/Source/MetabolicEfficiencyUncapped-BS/HarmonyPatches/CompProperties_IncorporateEffect _Patch.cs
@@ -0,0 +1,17 @@
+﻿using BigAndSmall;
+using HarmonyLib;
+using Verse;
+
+namespace MetabolicEfficiencyUncapped.BS.HarmonyPatches;
+
+[HarmonyPatch(typeof(CompProperties_IncorporateEffect))]
+public static class CompProperties_IncorporateEffect__Patch
+{
+    [HarmonyPatch(nameof(CompProperties_IncorporateEffect.RemoveGenesOverLimit))]
+    [HarmonyPrefix]
+    public static bool RemoveGenesOverLimit(CompProperties_IncorporateEffect __instance, ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+}
